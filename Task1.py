04132eae8afc02737f1ff7b50e9549e24d3fc407
Natahis <natahis@bk.ru>
2022-10-02 01:41:40 +0300

    Task1 new

--- Task1.py
+++ Task1.py
@@ -5,11 +5,10 @@
 # - 6782 -> 23
 # - 0,56 -> 11
 
-number = float(input('Введите любое вещественное число: '))
-if number < 0: number = number * (-1)
+number = (input('Введите любое вещественное число: '))
 sum = 0
 for i in str(number):
-        if i != '.':
+        if i != '.' and i != ',' and i != '-':
             sum += int(i)
 
 print(F'{number} -> {sum}')
